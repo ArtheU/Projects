bb697789a040c334d37cd263193986f886c2cb0c
ArtheU <zibelolivier@gmail.com>
2022-02-09 16:51:34 +0100

    Green Garden

--- ExercicesJavaScript/ToDo List/assets/js/main.js
+++ ExercicesJavaScript/ToDo List/assets/js/main.js
@@ -9,7 +9,8 @@ btnAdd.addEventListener('click', function(){
     (document.getElementById('toDo').value != "") ? document.querySelector('ul').insertAdjacentHTML('beforeend', '<li class="item"><p>' + value + '</p> <button> poubelle </button> </li>') : alert('Veuillez insérer une nouvelle tâche !');
     document.getElementById('toDo').value = "";
     document.querySelectorAll('li').forEach(element => {
-        element.querySelector('button').addEventListener('click',function(){
+        element.querySelector('button').addEventListener('click',function(){    
+            confirm('az');
             remove(element);
         });
     });
