190f3fb243c1161020dfefd3b902e495707c37b2
Arthe_U <zibelolivier@gmail.com>
2022-02-27 16:52:23 +0100

    ECF v3

--- ECF/assets/js/main.js
+++ ECF/assets/js/main.js
@@ -75,3 +75,31 @@ for (let index = 0; index < delandpayDiv.length; index++) {
     element.style.background = delandpay[index];
     
 }
+
+/* ------------------------------SIGN UP FORM------------------------------ */
+function validInvalid(select, valid){
+    if(valid === true){
+        document.querySelector(select).classList.remove('is-invalid');
+        document.querySelector(select).classList.toggle('is-valid');
+    }else{
+        document.querySelector(select).classList.remove('is-valid');
+        document.querySelector(select).classList.toggle('is-invalid');
+    }
+}
+
+document.querySelector('.lastName').addEventListener('change', function(){
+    (document.querySelector('.lastName').value.length > 2) ? validInvalid('.lastName', true) : validInvalid('.lastName', false);
+});
+
+document.querySelector('.firstName').addEventListener('change', function(){
+    (document.querySelector('.firstName').value.length > 2) ? validInvalid('.firstName', true) : validInvalid('.firstName', false);
+});
+
+document.querySelector('.passwordConfirm').addEventListener('change', function(){
+    (document.querySelector('.password').value === document.querySelector('.passwordConfirm').value) ? validInvalid('.passwordConfirm', true) : validInvalid('.passwordConfirm', false);
+});
+
+document.querySelector('.btn-signup').addEventListener('click',function(){
+    (document.querySelectorAll('.is-invalid').length == 0 && document.querySelectorAll('.is-valid').length == 3) ? alert('Gucci') : alert('Error');
+});
+
